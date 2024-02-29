39b5c2758cdd45c93d80efa8d7a723253b0ffa47
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:04:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var alien_color = "green";
+if (alien_color == "green")
+    console.log("you earn 5 points");
---
+++ main.ts
@@ -0,0 +1,4 @@
+let alien_color = "green"
+
+if(alien_color == "green")
+console.log("you earn 5 points")
